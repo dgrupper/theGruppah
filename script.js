bc9661c2878cdb2716dfe04051bba49977fc24fa
David Grupper <46638379+dgrupper@users.noreply.github.com>
2019-03-29 01:33:58 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,5 @@
 // This project requires at least one custom JavaScript function
-//
+// jquery is fine to use
+
+const yes = "The mortgage payment is less than one third of your monthly income, you may accept this loan.";
+$('yesNo').text(yes);
